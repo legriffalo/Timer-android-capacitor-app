5a07d729f3d1d21c88a72770d9d7a778ccbd94a0
Nick-legriffalo-Grif <56260217+legriffalo@users.noreply.github.com>
2024-11-21 22:10:37 +0200

    First ever capacitor to APK build
    - run through was good, mainly hiccup free
    - need logo on next run
    - need to look in to using native functionality cam etc for next project

---
+++ android/app/src/main/java/my/stopwatch/MainActivity.java
@@ -0,0 +1,5 @@
+package my.stopwatch;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'my.stopwatch',
+  appName: 'stopwatch',
+  webDir: 'dist'
+};
+
+export default config;
